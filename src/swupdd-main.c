0b81cd03026cecd8e11a7e66db11e7d4e8a9f72f
Jussi Laako <jussi.laako@linux.intel.com>
2016-02-22 17:30:25 +0200

    Add initial build infra

---
+++ src/swupdd-main.c
@@ -0,0 +1,15 @@
+/*
+ *
+ * Daemon for controlling Clear Linux Software Update Client
+ * Copyright (C) 2016 Intel Corporation
+ *
+ * Author: Jussi Laako <jussi.laako@linux.intel.com>
+ *
+ */
+
+
+int main (int argc, char *argv[])
+{
+	return 0;
+}
+
